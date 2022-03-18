436a8fcc87ac271db4e86a57cc73cf26ccd45046
Edwin Marroquin <investigacion.tec.ud@gmail.com>
2022-03-18 15:03:37 -0500

    UPDATE: Update workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hydro/",
+  base: "https://inteud.github.io/hydro/",
   plugins: [vue()],
   resolve: {
     alias: {
