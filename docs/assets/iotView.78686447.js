4d5a0516663fc9b2099e100832ceed156a3a19b3
Edwin Marroquin <devem.software@gmail.com>
2022-03-20 21:15:35 -0500

    FIXED: Fixed vue-router  (T _ T)

---
+++ docs/assets/iotView.78686447.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.2e79fd1b.js";import{o,c as r}from"./vendor.24c6ff63.js";const t={};function c(a,n,s,p,_,f){return o(),r("h1",null,"IOT")}var m=e(t,[["render",c]]);export{m as default};
